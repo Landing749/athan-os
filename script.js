d10a2ec982614216f138bc4e1bd52a5bc7021945
Landing749 <arhanmeir.obrero@gmail.com>
2025-07-11 21:37:39 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+// Memory core boot sequence
+// They told me not to run this function again. I did anyway.
 
+console.log("ATHAN.OS: Initialization complete.");
+
+let boot = true;
+if (boot) {
+  console.log("You found the log. That means you're one of us.");
+}
